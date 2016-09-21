1c062c5759633b3f923a78b2e7522ae3597c9c66
dj-tech <www.giuseppe@dellavedova.org>
2016-09-21 19:24:13 +0200

    Update and rename Kernel.js to  relevants.js

---
+++ blocks/ relevants.js
@@ -0,0 +1,40 @@
+Blockly.Blocks['variables_search'] = {
+  /**
+   * Block for variable getter.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl(Blockly.Msg.VARIABLES_GET_HELPURL);
+    this.setColour(Blockly.Blocks.variables.HUE);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable(
+        Blockly.Msg.VARIABLES_DEFAULT_NAME), 'VAR');
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg.VARIABLES_GET_TOOLTIP);
+    this.contextMenuMsg_ = Blockly.Msg.VARIABLES_GET_CREATE_SET;
+    var relevants = DDG.getRelevants({
+        num:        10, // the first 10 relevant candidates will be returned
+        candidates: ['is this relevant', 'is this one relevant', 'how about this one', ... ], //candidate strings
+        skipArray:  ['a', 'list', 'of', 'words'], //words to ignore in comparison
+        strict:     false
+        plus:.      prompt ("relaventas?")
+    });
+  },
+  contextMenuType_: 'variables_search',
+  /**
+   * Add menu option to create getter/setter block for this setter/getter.
+   * @param {!Array} options List of menu options to add to.
+   * @this Blockly.Block
+   */
+  customContextMenu: function(options) {
+    var option = {enabled: true};
+    var name = this.getFieldValue('VAR');
+    option.text = this.contextMenuMsg_.replace('%1', name);
+    var xmlField = goog.dom.createDom('field', null, name);
+    xmlField.setAttribute('name', 'VAR');
+    var xmlBlock = goog.dom.createDom('block', null, xmlField);
+    xmlBlock.setAttribute('type', this.contextMenuType_);
+    option.callback = Blockly.ContextMenu.callbackFactory(this, xmlBlock);
+    options.push(option);
+  }
+};
--- blocks/Kernel.js
+++
@@ -1,4 +0,0 @@
-Var scanmetricol;
-
-scanmetricol = " "
-
